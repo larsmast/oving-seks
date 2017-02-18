820b5b1c3709c403a3481f90f3b091f18f3cea61
larsmast <larsmast@stud.ntnu.no>
2017-02-18 14:23:40 +0100

    update 1

---
+++ handIn6/Car.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include "Car.h"
+
+
+
+Car::Car()
+{
+public:
+	bool hasFreeSeats();
+
+private:
+	int freeSeats;				// Number of free seats
+
+
+}
+
+
+Car::~Car()
+{
+}
